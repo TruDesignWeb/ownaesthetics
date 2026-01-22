39b1805d8aa0604a9a897c7feb285cb2ae6c8dac
Truman-Folkers <trumanjackm@gmail.com>
2026-01-22 13:51:14 -0600

    adding social links

--- src/components/social-media.jsx
+++ src/components/social-media.jsx
@@ -5,10 +5,10 @@ const SocialMedia = () => {
     return (
         <div className="social-media-container">
             <p className = "social-media-title">Follow us on Social Media!</p>
-            <a href="https://www.facebook.com/YourPage" target="_blank" rel="noopener noreferrer" className="social-media-icon">
+            <a href="https://www.facebook.com/p/Own-Aesthetics-61576022630962/" target="_blank" rel="noopener noreferrer" className="social-media-icon">
                 <img src = "/assets/images/images/instagram-icon"></img>
             </a>
-            <a href="https://www.instagram.com/YourProfile" target="_blank" rel="noopener noreferrer" className="social-media-icon">
+            <a href="https://www.instagram.com/ownaestheticsdsm/" target="_blank" rel="noopener noreferrer" className="social-media-icon">
                 <img src = "/assets/images/images/instagram-icon"></img>
 
             </a>
