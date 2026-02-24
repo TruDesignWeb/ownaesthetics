6c8945578d1c67804b841f8d6f3cb87115471541
truman <trudesignweb@gmail.com>
2026-02-24 05:28:36 -0600

    antiwrinkle clanssname fix

--- src/pages/services/antiwrinkle.jsx
+++ src/pages/services/antiwrinkle.jsx
@@ -17,25 +17,28 @@ export default function AntiWrinkle() {
             <Banner3
             image="/assets/images/images/nic-y-c-It0vaZdSDDw-unsplash.jpg"
             
-            title = "Anti-Wrinkle Injections"
-            subtitle = "Smooth and Rejuvenate Your Skin with Our Expert Anti-Wrinkle Injections"
+            title="Anti-Wrinkle Injections"
+            subtitle="Smooth and Rejuvenate Your Skin with Our Expert Anti-Wrinkle Injections"
             />
 
-            <section id = "services">
-                <div class = "container">
-                    <h1 class = "title servces-animated">Anti-Wrinkle Injections</h1>
-                    <div class = "row">
-                        <div class = "col-md-auto services-text service-boxes animated">
-                            <p>BOTOX ® Cosmetic, Dysport, and other neurotoxins are prescription medicines that are injected
+            <section id="antiwrinkle-services">
+                <div className="antiwrinkle-container">
+                    <h1 className="antiwrinkle-title antiwrinkle-animated">
+                        Anti-Wrinkle Injections
+                    </h1>
+                    <div className="antiwrinkle-row">
+                        <div className="antiwrinkle-services-text antiwrinkle-service-boxes antiwrinkle-animated">
+                            <p>
+                                BOTOX ® Cosmetic, Dysport, and other neurotoxins are prescription medicines that are injected
                                 into muscles and used to temporarily soften and improve the look of moderate to severe
                                 forehead lines, crow’s-feet lines, frown lines between the eyebrows, and vertical bands
-                                connecting the jaw and neck (platysma bands) in adults.<br></br><br></br>
+                                connecting the jaw and neck (platysma bands) in adults.<br /><br />
                                 Treatment results last an average of 3-5 months. It is best to schedule maintenance treatments
-                                when you see more movement in your expression lines.<br></br><br></br>
-                                Treatment Areas:<br></br><br></br>
-                                Upper Face<br></br><br></br>
-                                Lower Face<br></br><br></br>
-                                Neck<br></br><br></br>
+                                when you see more movement in your expression lines.<br /><br />
+                                Treatment Areas:<br /><br />
+                                Upper Face<br /><br />
+                                Lower Face<br /><br />
+                                Neck<br /><br />
                                 Sessions: 30 minutes
                             </p>
                         </div>
@@ -43,7 +46,6 @@ export default function AntiWrinkle() {
                 </div>
             </section>
 
-            
             <Footer />
         </>
     );
