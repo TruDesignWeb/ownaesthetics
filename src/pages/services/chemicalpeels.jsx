7b88435587081ada1831bdc2573ae87a38552e4f
truman <trudesignweb@gmail.com>
2026-02-24 04:02:32 -0600

    chem peels

---
+++ src/pages/services/chemicalpeels.jsx
@@ -0,0 +1,52 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from '../../components/navbar';
+import Footer from '../../components/footer';
+import Banner2 from '../../components/banner2';
+import Banner3 from '../../components/banner3';
+import CustomCursor from '../../components/customcursor';
+import './chemicalpeels.css';
+
+
+export default function Chemicalpeels() {
+    return (
+        <>
+            <CustomCursor />
+            <Navbar />
+            
+            <Banner3
+            image="/assets/images/images/nic-y-c-It0vaZdSDDw-unsplash.jpg"
+            
+            title = "Chemical Peels"
+            subtitle = "Reveal Your Radiance with Our Expert Chemical Peels"
+            />
+
+            <section id = "services">
+                <div class = "container">
+                    {/* <h1 class = "title servces-animated">Chemical Peels</h1> */}
+                    <div class = "row">
+                        <div class = "col-md-auto services-text service-boxes animated">
+                            <p>We get it, ‘acid’, ‘peel’, and ‘chemical’ can sound a bit scary. Did you know that there is a
+                                chemical peel for the most sensitive skin types? You can choose to have no downtime.<br/><br/>
+                                Chemical peels are a minimally invasive and affordable way to dramatically improve the
+                                appearance of skin. They help diminish the appearance of fine lines and wrinkles. They also
+                                work to improve the appearance of discoloration, uneven skin tone, and sun damage because
+                                the peel action removes damaged, dead skin. Your skin could look younger and plumper after
+                                just seven days.<br/><br/>
+                                Treatment Area:<br/><br/>
+                                Face<br/><br/>
+                                Neck<br/><br/>
+                                Décolleté<br/><br/>
+                                Back<br/><br/>
+                                Sessions: 25 min- 1 hr.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            
+            <Footer />
+        </>
+    );
+}
