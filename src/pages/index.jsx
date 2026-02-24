aef53e79cf74c739fbd9dbdeef2d247277a8cbbc
truman <trudesignweb@gmail.com>
2026-02-24 05:59:52 -0600

    trying to fix showcase section - it works but sum off

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -106,7 +106,18 @@ export default function Home() {
                   cards={featureCards}
                   variant="" // Optional: for different styles
                 />
-                
+                <ShowcaseSection 
+                reverse={true}
+          eyebrow=""
+          buttonLink="services/saltfacials"
+          title="Our Vision"
+          text="Own Aesthetics is a medical spa born from a desire to serve those who want to Own their Aesthetic. We desire to serve those who embrace life’s journey while enhancing that experience. With knowledgeable expertise, we offer the best and latest options for skin health, sorting trends from reliable solutions."
+          buttonLabel="Learn More"
+          imageSrc=""
+          imageAlt="MRI imaging suite"
+          backgroundColor={"#e8e0d1"}
+              />
+
                 <ShowcaseSection 
           eyebrow="Facial Treatments"
           title="NEW Salt Facials"
