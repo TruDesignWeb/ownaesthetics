24c8ae8cad5e723b7a74d075eb2f85fe55cd96c1
truman <trudesignweb@gmail.com>
2026-02-24 05:38:50 -0600

    css fix

--- src/pages/services/juvederm.jsx
+++ src/pages/services/juvederm.jsx
@@ -15,18 +15,20 @@ export default function Juvederm() {
             <Navbar />
             
             <Banner3
-            image="/assets/images/images/nic-y-c-It0vaZdSDDw-unsplash.jpg"
-            
-            title = "Facial Contouring - Juvederm Collection of Injectable HA Dermal Fillers"
-            subtitle = "Enhance Your Natural Beauty with the Juvederm Collection of Injectable HA Dermal Fillers"
+                image="/assets/images/images/nic-y-c-It0vaZdSDDw-unsplash.jpg"
+                title="Facial Contouring - Juvederm Collection of Injectable HA Dermal Fillers"
+                subtitle="Enhance Your Natural Beauty with the Juvederm Collection of Injectable HA Dermal Fillers"
             />
 
-            <section id = "services">
-                <div class = "container">
-                    <h1 class = "title servces-animated">Facial Contouring - Juvederm Collection of Injectable HA Dermal Fillers</h1>
-                    <div class = "row">
-                        <div class = "col-md-auto services-text service-boxes animated">
-                            <p>Injectable dermal fillers are approved treatments that temporarily add volume to certain areas
+            <section id="juvederm-services">
+                <div className="juvederm-container">
+                    <h1 className="juvederm-title juvederm-animated">
+                        Facial Contouring - Juvederm Collection of Injectable HA Dermal Fillers
+                    </h1>
+                    <div className="juvederm-row">
+                        <div className="juvederm-services-text juvederm-service-boxes juvederm-animated">
+                            <p>
+                                Injectable dermal fillers are approved treatments that temporarily add volume to certain areas
                                 of the face, including the cheeks, lips, and nasolabial folds. They are made from various
                                 substances, but the primary ingredient in most injectable fillers is hyaluronic acid (HA), a water-
                                 loving molecule found naturally in the skin. A modified form of HA is the backbone of many
@@ -38,7 +40,6 @@ export default function Juvederm() {
                 </div>
             </section>
 
-            
             <Footer />
         </>
     );
--- src/pages/services/kybella.jsx
+++ src/pages/services/kybella.jsx
@@ -15,36 +15,37 @@ export default function Kybella() {
             <Navbar />
             
             <Banner3
-            image="/assets/images/images/nic-y-c-It0vaZdSDDw-unsplash.jpg"
-            
-            title = "Kybella®"
-            subtitle = "Sculpt Your Profile with Kybella® - The Only FDA-Approved Injectable for Double Chin Reduction"
+                image="/assets/images/images/nic-y-c-It0vaZdSDDw-unsplash.jpg"
+                title="Kybella®"
+                subtitle="Sculpt Your Profile with Kybella® - The Only FDA-Approved Injectable for Double Chin Reduction"
             />
 
-            <section id = "services">
-                <div class = "container">
-                        <h1 class = "title servces-animated">Kybella®</h1>
-                        <div class = "row">
-                            <div class = "col-md-auto services-text service-boxes animated">
-                                <p>KYBELLA® is the only FDA-approved injectable treatment for adults that destroys fat cells to
-                                    gradually improve the appearance and profile of moderate to severe fat below the chin
-                                    (submental fat), also called “double chin.”<br/><br/>
-                                    The deoxycholic acid in Kybella disrupts the fat cell&#39;s membrane, causing them to be broken
-                                    down and naturally eliminated by the body. This gradually reduces submental fat, leading to a
-                                    more defined chin line and a smoother, more sculpted profile. The results are generally
-                                    permanent as the destroyed fat cells do not regenerate. <br/><br/>
-                                    Multiple treatments may be needed to achieve the desired results, with some patients seeing
-                                    noticeable improvements after just a few sessions.<br/><br/>
-                                    Treatment Area:<br/><br/>
-                                    Neck<br/><br/>
-                                    Sessions: 45 min. – 1hr.
-                                </p>
-                            </div>
+            <section id="kybella-services">
+                <div className="kybella-container">
+                    <h1 className="kybella-title kybella-animated">
+                        Kybella®
+                    </h1>
+                    <div className="kybella-row">
+                        <div className="kybella-services-text kybella-service-boxes kybella-animated">
+                            <p>
+                                KYBELLA® is the only FDA-approved injectable treatment for adults that destroys fat cells to
+                                gradually improve the appearance and profile of moderate to severe fat below the chin
+                                (submental fat), also called “double chin.”<br/><br/>
+                                The deoxycholic acid in Kybella disrupts the fat cell&#39;s membrane, causing them to be broken
+                                down and naturally eliminated by the body. This gradually reduces submental fat, leading to a
+                                more defined chin line and a smoother, more sculpted profile. The results are generally
+                                permanent as the destroyed fat cells do not regenerate.<br/><br/>
+                                Multiple treatments may be needed to achieve the desired results, with some patients seeing
+                                noticeable improvements after just a few sessions.<br/><br/>
+                                Treatment Area:<br/><br/>
+                                Neck<br/><br/>
+                                Sessions: 45 min. – 1hr.
+                            </p>
                         </div>
                     </div>
-                </section>
+                </div>
+            </section>
 
-            
             <Footer />
         </>
     );
