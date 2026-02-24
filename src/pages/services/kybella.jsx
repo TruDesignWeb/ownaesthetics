2266d82c3250131b594f9215f100c4445efc92cb
truman <trudesignweb@gmail.com>
2026-02-24 04:16:04 -0600

    kyky

---
+++ src/pages/services/kybella.jsx
@@ -0,0 +1,51 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from '../../components/navbar';
+import Footer from '../../components/footer';
+import Banner2 from '../../components/banner2';
+import Banner3 from '../../components/banner3';
+import CustomCursor from '../../components/customcursor';
+import './kybella.css';
+
+
+export default function Kybella() {
+    return (
+        <>
+            <CustomCursor />
+            <Navbar />
+            
+            <Banner3
+            image="/assets/images/images/nic-y-c-It0vaZdSDDw-unsplash.jpg"
+            
+            title = "Kybella®"
+            subtitle = "Sculpt Your Profile with Kybella® - The Only FDA-Approved Injectable for Double Chin Reduction"
+            />
+
+            <section id = "services">
+                <div class = "container">
+                        <h1 class = "title servces-animated">Kybella®</h1>
+                        <div class = "row">
+                            <div class = "col-md-auto services-text service-boxes animated">
+                                <p>KYBELLA® is the only FDA-approved injectable treatment for adults that destroys fat cells to
+                                    gradually improve the appearance and profile of moderate to severe fat below the chin
+                                    (submental fat), also called “double chin.”<br/><br/>
+                                    The deoxycholic acid in Kybella disrupts the fat cell&#39;s membrane, causing them to be broken
+                                    down and naturally eliminated by the body. This gradually reduces submental fat, leading to a
+                                    more defined chin line and a smoother, more sculpted profile. The results are generally
+                                    permanent as the destroyed fat cells do not regenerate. <br/><br/>
+                                    Multiple treatments may be needed to achieve the desired results, with some patients seeing
+                                    noticeable improvements after just a few sessions.<br/><br/>
+                                    Treatment Area:<br/><br/>
+                                    Neck<br/><br/>
+                                    Sessions: 45 min. – 1hr.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            
+            <Footer />
+        </>
+    );
+}
