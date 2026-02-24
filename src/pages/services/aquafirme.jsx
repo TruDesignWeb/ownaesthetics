514e0e3a756a29a47bc2b32ad9276596fb4453f3
truman <trudesignweb@gmail.com>
2026-02-24 05:31:37 -0600

    aquafirm css fix

--- src/pages/services/aquafirme.jsx
+++ src/pages/services/aquafirme.jsx
@@ -15,43 +15,44 @@ export default function Aquafirme() {
             <Navbar />
             
             <Banner3
-            image="/assets/images/images/nic-y-c-It0vaZdSDDw-unsplash.jpg"
-            
-            title = "AquaFirmeXS"
-            subtitle = "Experience the Ultimate Skin Rejuvenation with AquaFirmeXS: A Revolutionary Treatment for Radiant, Youthful Skin"
+                image="/assets/images/images/nic-y-c-It0vaZdSDDw-unsplash.jpg"
+                title="AquaFirmeXS"
+                subtitle="Experience the Ultimate Skin Rejuvenation with AquaFirmeXS: A Revolutionary Treatment for Radiant, Youthful Skin"
             />
 
-            <section id = "services">
-                <div class="content-wrapper">
-                    <h1 class="title services-animated">AquaFirmeXS</h1>
+            <section id="aquafirme-services">
+                <div className="aquafirme-content-wrapper">
+                    <h1 className="aquafirme-title aquafirme-animated">
+                        AquaFirmeXS
+                    </h1>
                     
-                    <div class="main-content">
-                        <div class="image-gallery">
+                    <div className="aquafirme-main-content">
+                        <div className="aquafirme-image-gallery">
                             <img src="/assets/images/images/DERIVE 2023 Presentation copy-19 (dragged).JPG" alt="Salt Facial Before & After 1"/>
                             <img src="/assets/images/images/ABB4D0E0-2BF2-4275-A151-655022AC1C04.JPG" alt="Salt Facial Before & After 2"/>
                             <img src="/assets/images/images/2136DDF0-9B26-4CAE-952F-DA37AE07590D.JPG" alt="Salt Facial Before & After 3"/>
                         </div>
                         
-                        <div class="content-text animated">
-                            
-                                                <p><strong>AquaFirmeXS</strong> is safe and effective with zero downtime 
-                            creating noticeable improvements, and it is one of the most popular treatments on the market.<br/><br/> The customizable treatment protocols address 
-                            uneven complexion, acne-prone skin, blemishes, congestion, texture, and tone.
-                            The combination of micro-cupping, vibrating massage, and ultrasound provide 
-                            a comprehensive treatment to help exfoliate, cleanse, and revitalize the skin.
-                            Our plant-based patent-pending serum used with AquaFirmeXS help reenergize the 
-                            skin cells by harnessing the body’s own power to fight against the most common 
-                            skin concerns.<br/><br/> In addition, AquaFirmeXS provides a unique combination of serums
-                            to boost scalp and hair health. The combination of our <strong>DETOXTM</strong> serum and <strong>DE|RIVETM</strong>
-                            topical are specifically designed to reduce inflammation and reenergize the hair 
-                            follicles.</p>
+                        <div className="aquafirme-content-text aquafirme-animated">
+                            <p>
+                                <strong>AquaFirmeXS</strong> is safe and effective with zero downtime 
+                                creating noticeable improvements, and it is one of the most popular treatments on the market.<br/><br/>
+                                The customizable treatment protocols address uneven complexion, acne-prone skin, blemishes, congestion, texture, and tone.
+                                The combination of micro-cupping, vibrating massage, and ultrasound provide 
+                                a comprehensive treatment to help exfoliate, cleanse, and revitalize the skin.
+                                Our plant-based patent-pending serum used with AquaFirmeXS help reenergize the 
+                                skin cells by harnessing the body’s own power to fight against the most common 
+                                skin concerns.<br/><br/>
+                                In addition, AquaFirmeXS provides a unique combination of serums
+                                to boost scalp and hair health. The combination of our <strong>DETOXTM</strong> serum and <strong>DE|RIVETM</strong>
+                                topical are specifically designed to reduce inflammation and reenergize the hair 
+                                follicles.
+                            </p>
                         </div>
                     </div>
                 </div>
             </section>
 
-
-            
             <Footer />
         </>
     );
