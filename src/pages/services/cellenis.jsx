b8af6bd3f505c3ed219494a0c9b776bbf25768f5
truman <trudesignweb@gmail.com>
2026-03-31 12:39:18 -0500

    images for cellenis prp/prf

--- src/pages/services/cellenis.jsx
+++ src/pages/services/cellenis.jsx
@@ -1,52 +1,67 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Navbar from '../../components/navbar';
-import Footer from '../../components/footer';
-import Banner2 from '../../components/banner2';
-import Banner3 from '../../components/banner3';
-import CustomCursor from '../../components/customcursor';
-import './cellenis.css';
-
+import Navbar from "../../components/navbar";
+import Footer from "../../components/footer";
+import Banner2 from "../../components/banner2";
+import Banner3 from "../../components/banner3";
+import CustomCursor from "../../components/customcursor";
+import "./cellenis.css";
 
 export default function Cellenis() {
-    return (
-        <>
-            <CustomCursor />
-            <Navbar />
-            
-            <Banner3
-                image="/assets/images/images/sumner-mahaffey-7Y0NshQLohk-unsplash.jpg"
-                title="Cellenis® Derma PRP/PRF"
-                subtitle="Rejuvenate Your Skin with Cellenis® Derma PRP/PRF: A Natural Approach to Radiant, Youthful Skin"
-            />
+  return (
+    <>
+      <CustomCursor />
+      <Navbar />
 
-            <section id="cellenis-services">
-                <div className="cellenis-container">
-                    <h1 className="cellenis-title cellenis-animated">
-                        {/* Cellenis® Derma PRP */}
-                    </h1>
-                    <div className="cellenis-row">
-                        <div className="cellenis-services-text cellenis-service-boxes cellenis-animated">
-                            <p>
-                                Cellenis® Derma PRP is a skin rejuvenation treatment that uses Platelet-Rich Plasma (PRP)
-                                derived from the patient’s blood to stimulate collagen production and improve skin texture.
-                                The process begins with a small blood draw, which is then spun in a centrifuge to separate the
-                                PRP. The concentrated plasma contains abundant growth factors, which are injected into
-                                targeted areas to promote healing and regeneration. Cellenis® Derma PRP can treat areas such
-                                as the face, neck, hands, and décolletage, helping to reduce fine lines, wrinkles, and scars.
-                                Results can last up to 18 months.<br/><br/>
-                                
-                                Treatment Area:<br/><br/>
-                                Upper Face<br/><br/>
-                                Lower Face<br/><br/>
-                                Sessions: 30 min – 1 hr.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+      <Banner3
+        image="/assets/images/images/sumner-mahaffey-7Y0NshQLohk-unsplash.jpg"
+        title="Cellenis® Derma PRP/PRF"
+        subtitle="Rejuvenate Your Skin with Cellenis® Derma PRP/PRF: A Natural Approach to Radiant, Youthful Skin"
+      />
 
-            <Footer />
-        </>
-    );
+      <section id="cellenis-services">
+        <div className="cellenis-container">
+          <h1 className="cellenis-title cellenis-animated">
+            {/* Cellenis® Derma PRP */}
+          </h1>
+          <div className="cellenis-row">
+            <div className="cellenis-image-gallery">
+              <img
+                src="/assets/images/prfbefore+after.jpeg"
+                alt="Cellenis Before & After"
+              />
+            </div>
+            <div className="cellenis-services-text cellenis-service-boxes cellenis-animated">
+              <p>
+                Cellenis® Derma PRP is a skin rejuvenation treatment that uses
+                Platelet-Rich Plasma (PRP) derived from the patient’s blood to
+                stimulate collagen production and improve skin texture. The
+                process begins with a small blood draw, which is then spun in a
+                centrifuge to separate the PRP. The concentrated plasma contains
+                abundant growth factors, which are injected into targeted areas
+                to promote healing and regeneration. Cellenis® Derma PRP can
+                treat areas such as the face, neck, hands, and décolletage,
+                helping to reduce fine lines, wrinkles, and scars. Results can
+                last up to 18 months.
+                <br />
+                <br />
+                Treatment Area:
+                <br />
+                <br />
+                Upper Face
+                <br />
+                <br />
+                Lower Face
+                <br />
+                <br />
+                Sessions: 30 min – 1 hr.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <Footer />
+    </>
+  );
 }
