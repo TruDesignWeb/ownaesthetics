2768b440266d03f7e62364737502f1b7aafbfab6
truman <trudesignweb@gmail.com>
2026-03-31 12:35:28 -0500

    formatting for antiwrinkle and kybella images done

--- src/pages/services/antiwrinkle.jsx
+++ src/pages/services/antiwrinkle.jsx
@@ -1,60 +1,80 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Navbar from '../../components/navbar';
-import Footer from '../../components/footer';
-import Banner2 from '../../components/banner2';
-import Banner3 from '../../components/banner3';
-import CustomCursor from '../../components/customcursor';
-import './antiwrinkle.css';
-
+import Navbar from "../../components/navbar";
+import Footer from "../../components/footer";
+import Banner2 from "../../components/banner2";
+import Banner3 from "../../components/banner3";
+import CustomCursor from "../../components/customcursor";
+import "./antiwrinkle.css";
 
 export default function AntiWrinkle() {
-    return (
-        <>  
-        <CustomCursor />
-            <Navbar />
-            
-            <Banner3
-            image="/assets/images/images/sumner-mahaffey-7Y0NshQLohk-unsplash.jpg"
-            
-            title="Anti-Wrinkle Injections"
-            subtitle="Smooth and Rejuvenate Your Skin with Our Expert Anti-Wrinkle Injections"
-            />
+  return (
+    <>
+      <CustomCursor />
+      <Navbar />
 
-            <section id="antiwrinkle-services">
-                
-                <div className="antiwrinkle-container">
-                    {/* <h1 className="antiwrinkle-title antiwrinkle-animated">
-                         Anti-Wrinkle Injections 
+      <Banner3
+        image="/assets/images/images/sumner-mahaffey-7Y0NshQLohk-unsplash.jpg"
+        title="Anti-Wrinkle Injections"
+        subtitle="Smooth and Rejuvenate Your Skin with Our Expert Anti-Wrinkle Injections"
+      />
+
+      <section id="antiwrinkle-services">
+        <div className="antiwrinkle-container">
+          {/* <h1 className="antiwrinkle-title antiwrinkle-animated">
+                        Anti-Wrinkle Injections 
                     </h1> */}
-                     
-                    <div className="antiwrinkle-row">
 
-                        <div className="antiwrinkle-image-gallery">
-                            <img src="/assets/images/antiwrinklebefore+after.jpeg" alt="Antiwrinkle Before & After"/>
-                            <img src="/assets/images/antiwrinklebefore+after2.jpeg" alt="Antiwrinkle before and After"/>
-                            <img src="/assets/images/antiwrinklebefore+after3.jpeg" alt="Antiwrinkle Before and After"/>
-                        </div>
-                        <div className="antiwrinkle-services-text antiwrinkle-service-boxes antiwrinkle-animated">
-                            <p>
-                                BOTOX ® Cosmetic, Dysport, and other neurotoxins are prescription medicines that are injected
-                                into muscles and used to temporarily soften and improve the look of moderate to severe
-                                forehead lines, crow’s-feet lines, frown lines between the eyebrows, and vertical bands
-                                connecting the jaw and neck (platysma bands) in adults.<br /><br />
-                                Treatment results last an average of 3-5 months. It is best to schedule maintenance treatments
-                                when you see more movement in your expression lines.<br /><br />
-                                Treatment Areas:<br /><br />
-                                Upper Face<br /><br />
-                                Lower Face<br /><br />
-                                Neck<br /><br />
-                                Sessions: 30 minutes
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+          <div className="antiwrinkle-row">
+            <div className="antiwrinkle-image-gallery">
+              <img
+                src="/assets/images/antiwrinklebefore+after.jpeg"
+                alt="Antiwrinkle Before & After"
+              />
+              <img
+                src="/assets/images/antiwrinklebefore+after2.jpeg"
+                alt="Antiwrinkle before and After"
+              />
+              <img
+                src="/assets/images/antiwrinklebefore+after3.jpeg"
+                alt="Antiwrinkle Before and After"
+              />
+            </div>
+            <div className="antiwrinkle-services-text antiwrinkle-service-boxes antiwrinkle-animated">
+              <p>
+                BOTOX ® Cosmetic, Dysport, and other neurotoxins are
+                prescription medicines that are injected into muscles and used
+                to temporarily soften and improve the look of moderate to severe
+                forehead lines, crow’s-feet lines, frown lines between the
+                eyebrows, and vertical bands connecting the jaw and neck
+                (platysma bands) in adults.
+                <br />
+                <br />
+                Treatment results last an average of 3-5 months. It is best to
+                schedule maintenance treatments when you see more movement in
+                your expression lines.
+                <br />
+                <br />
+                Treatment Areas:
+                <br />
+                <br />
+                Upper Face
+                <br />
+                <br />
+                Lower Face
+                <br />
+                <br />
+                Neck
+                <br />
+                <br />
+                Sessions: 30 minutes
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
 
-            <Footer />
-        </>
-    );
+      <Footer />
+    </>
+  );
 }
--- src/pages/services/kybella.jsx
+++ src/pages/services/kybella.jsx
@@ -1,52 +1,69 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Navbar from '../../components/navbar';
-import Footer from '../../components/footer';
-import Banner2 from '../../components/banner2';
-import Banner3 from '../../components/banner3';
-import CustomCursor from '../../components/customcursor';
-import './kybella.css';
-
+import Navbar from "../../components/navbar";
+import Footer from "../../components/footer";
+import Banner2 from "../../components/banner2";
+import Banner3 from "../../components/banner3";
+import CustomCursor from "../../components/customcursor";
+import "./kybella.css";
 
 export default function Kybella() {
-    return (
-        <>
-            <CustomCursor />
-            <Navbar />
-            
-            <Banner3
-                image="/assets/images/images/sumner-mahaffey-7Y0NshQLohk-unsplash.jpg"
-                title="Kybella®"
-                subtitle="Sculpt Your Profile with Kybella® - The Only FDA-Approved Injectable for Double Chin Reduction"
-            />
+  return (
+    <>
+      <CustomCursor />
+      <Navbar />
 
-            <section id="kybella-services">
-                <div className="kybella-container">
-                    <h1 className="kybella-title kybella-animated">
-                        {/* Kybella® */}
-                    </h1>
-                    <div className="kybella-row">
-                        <div className="kybella-services-text kybella-service-boxes kybella-animated">
-                            <p>
-                                KYBELLA® is the only FDA-approved injectable treatment for adults that destroys fat cells to
-                                gradually improve the appearance and profile of moderate to severe fat below the chin
-                                (submental fat), also called “double chin.”<br/><br/>
-                                The deoxycholic acid in Kybella disrupts the fat cell&#39;s membrane, causing them to be broken
-                                down and naturally eliminated by the body. This gradually reduces submental fat, leading to a
-                                more defined chin line and a smoother, more sculpted profile. The results are generally
-                                permanent as the destroyed fat cells do not regenerate.<br/><br/>
-                                Multiple treatments may be needed to achieve the desired results, with some patients seeing
-                                noticeable improvements after just a few sessions.<br/><br/>
-                                Treatment Area:<br/><br/>
-                                Neck<br/><br/>
-                                Sessions: 45 min. – 1hr.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+      <Banner3
+        image="/assets/images/images/sumner-mahaffey-7Y0NshQLohk-unsplash.jpg"
+        title="Kybella®"
+        subtitle="Sculpt Your Profile with Kybella® - The Only FDA-Approved Injectable for Double Chin Reduction"
+      />
 
-            <Footer />
-        </>
-    );
+      <section id="kybella-services">
+        <div className="kybella-container">
+          <h1 className="kybella-title kybella-animated">{/* Kybella® */}</h1>
+          <div className="kybella-row">
+            <div className="kybella-image-gallery">
+              <img
+                src="/assets/images/kybellabefore+after.jpeg"
+                alt="Kybella Before & After"
+              />
+            </div>
+            <div className="kybella-services-text kybella-service-boxes kybella-animated">
+              <p>
+                KYBELLA® is the only FDA-approved injectable treatment for
+                adults that destroys fat cells to gradually improve the
+                appearance and profile of moderate to severe fat below the chin
+                (submental fat), also called “double chin.”
+                <br />
+                <br />
+                The deoxycholic acid in Kybella disrupts the fat cell&#39;s
+                membrane, causing them to be broken down and naturally
+                eliminated by the body. This gradually reduces submental fat,
+                leading to a more defined chin line and a smoother, more
+                sculpted profile. The results are generally permanent as the
+                destroyed fat cells do not regenerate.
+                <br />
+                <br />
+                Multiple treatments may be needed to achieve the desired
+                results, with some patients seeing noticeable improvements after
+                just a few sessions.
+                <br />
+                <br />
+                Treatment Area:
+                <br />
+                <br />
+                Neck
+                <br />
+                <br />
+                Sessions: 45 min. – 1hr.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <Footer />
+    </>
+  );
 }
