acaa0b7bd6d92eb21c20a51371ec2e0f1e363178
truman <trudesignweb@gmail.com>
2026-04-21 18:07:30 -0500

    allumier up top

--- src/pages/collabpartners.jsx
+++ src/pages/collabpartners.jsx
@@ -19,18 +19,6 @@ export default function ServiceHub() {
 
       {/* SECTION 1 */}
       <ShowcaseSection
-        eyebrow="Skin Health"
-        title="Skin Type Solutions"
-        text="Discover your personalized skincare routine based on your unique Baumann Skin TypeÂ®. Take the quiz and unlock targeted treatments."
-        buttonLabel="Take the Quiz"
-        buttonLink="https://skintypesolutions.com/pages/skin-type-quiz?code=OWNAESTHETICS"
-        imageSrc="/assets/images/images/stsimg.webp"
-        reverse={false}
-        backgroundColor="#f8f9fa"
-      />
-
-      {/* SECTION 2 */}
-      <ShowcaseSection
         eyebrow="Rejuvenation"
         title="AlumierMD "
         text="AlumierMD is a professionally dispensed skincare brand with a rapidly expanding global network of partner clinics. Our mission is to empower you to feel confident and powerful in your own skin. We achieve this by pushing the boundaries of skincare science and combining clinically proven ingredients with advanced technologies that deliver transformative results. Our goal is to connect you to our global community of trusted skincare professionals and unlock your skin's potential with a 360Â° approach to skin health."
@@ -39,6 +27,18 @@ export default function ServiceHub() {
         imageSrc="assets\images\images\wetransfer_photos_2025-04-09_1731\PHOTOS\HP 1 AlumierMD Pigmentation.jpg"
         reverse={true}
         backgroundColor="#ffffff"
+      />
+
+      {/* SECTION 2 */}
+      <ShowcaseSection
+        eyebrow="Skin Health"
+        title="Skin Type Solutions"
+        text="Discover your personalized skincare routine based on your unique Baumann Skin TypeÂ®. Take the quiz and unlock targeted treatments."
+        buttonLabel="Take the Quiz"
+        buttonLink="https://skintypesolutions.com/pages/skin-type-quiz?code=OWNAESTHETICS"
+        imageSrc="/assets/images/images/stsimg.webp"
+        reverse={false}
+        backgroundColor="#f8f9fa"
       />
 
       <Footer />
